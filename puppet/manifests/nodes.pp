3de6dd78d1ecd1182aa4f0187afee1575813c4e3
sunguangning <sunguangning@126.com>
2016-04-21 17:46:56 -0400

    modify nodes.pp

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -1,5 +1,5 @@
-node  'cookbook5'  {
-   file  {"/opt/ning/gitpuppet/puppet/tmp/hello":
+node  'cookbook6'  {
+   file  {"/opt/ning/tmp/hello11111111111111":
      content  =>  "hello gitpuppet world !";
    }
 }
