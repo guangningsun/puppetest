084340043fa38f121041b119f1a6ff3aac87daa7
sunguangning <sunguangning@126.com>
2016-04-22 19:34:51 -0400

    modify init.pp

--- puppet/modules/test/manifests/init.pp
+++ puppet/modules/test/manifests/init.pp
@@ -1,3 +1,3 @@
 class  test {  
-  file  {  "/opt/ning/tmp/$hostname":content  =>  '第4次测试puppet自动化安装module hello world !';  }
+  file  {  "/opt/ning/tmp/$hostname":content  =>  '第5次测试puppet自动化安装module hello world !\n';  }
 }
